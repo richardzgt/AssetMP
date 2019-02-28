439df0bdc8ac303675d428aac74c02cddcf4687c
高涛 <gaotao@huored.com>
2019-02-28 12:38:08 +0800

    add

--- AssetMP/urls.py
+++ AssetMP/urls.py
@@ -6,18 +6,13 @@ from vms.views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'AssetMP.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'AssetMP.views.dashboard', name='dashboard'),
-    # url(r'^cabinet/', cabinet.as_view(), name='cabinet'),
     url(r'^cabinet/', Cabinet.as_view(), name='cabinet'),
     url(r'^assets/', Assets.as_view(), name='assets'),
     url(r'^detail/', AssetDetail.as_view(), name='asset_detail'),
     url(r'^vmscreate/', VmsCreate.as_view(), name='vms_create'),
     url(r'^getCorp/', getCorp.as_view(), name='getCorp'),
     url(r'^test/', test.as_view(), name='test'),
-    url(r'^nginxadmin', include('nginxadmin.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
